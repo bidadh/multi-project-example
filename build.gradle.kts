944e077bfd7f10db7325f07bd5b5ce9c507c7b2e
Arthur Kazemi <bidadh@gmail.com>
2023-10-15 22:41:09 +1100

    cleanup

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,36 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
   idea
+  java
+  id("org.jetbrains.kotlin.jvm")
+}
+
+allprojects {
+  apply {
+    plugin("org.jetbrains.kotlin.jvm")
+  }
+
+  repositories {
+    mavenCentral()
+  }
+
+  java {
+    sourceCompatibility = JavaVersion.VERSION_20
+    toolchain {
+      languageVersion.set(JavaLanguageVersion.of(20))
+    }
+  }
+
+  tasks.withType<KotlinCompile> {
+    kotlinOptions {
+      jvmTarget = "20"
+      freeCompilerArgs += "-Xjsr305=strict"
+    }
+  }
+
+  tasks.withType<Test> {
+    // Use JUnit Platform for unit tests.
+    useJUnitPlatform()
+  }
 }

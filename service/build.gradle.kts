f0f5cc4badbeaa1eb03589450a1c8db04b38ad95
Arthur Kazemi <bidadh@gmail.com>
2023-10-17 06:33:48 +1100

    add sample task

--- service/build.gradle.kts
+++ service/build.gradle.kts
@@ -1,3 +1,10 @@
+import come.ideabaker.samples.gradle.tasks.StringDiffTask
+
 plugins {
   projectPlugin()
+}
+
+tasks.register<StringDiffTask>("stringDiff") {
+  string1.set("strin1")
+  string2.set("string2")
 }
